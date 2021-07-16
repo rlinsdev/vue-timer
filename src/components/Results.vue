1dfed14051da88714efc3565bdfce4257b883e68
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-16 08:07:36 -0300

    finishing - styling

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,14 +1,33 @@
 <template>
   <p>Reaction time: {{score}} ms</p>
+  <p class="rank">{{rank}}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank:null
+    }
+  },
+  mounted(){
+    if(this.score < 250){
+      this.rank = 'ninja fingers!'
+    } else if (this.score <4000){
+      this.rank = 'Rapid relexes'
+    } else {
+      this.rank = 'Snail peace'
+    }
+  }
 
 }
 </script>
 
 <style>
-
+.rank{
+  font-size: 1.4em;
+  color:#0fa;
+  font-weight:bold;
+}
 </style>
